347976a531f976b1099cd3f1a1f6294e25fafaa4
Unknown <patrickmonville@hotmail.com>
2016-12-02 15:22:33 +0100

    Base Character
    - movement and rotation of the character with Controller 1
    - rotation of the camera with Controller 2

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public float M_maxCameraAngle = 20f;
+
+    public Vector3 M_centerPos = new Vector3(0f, 1.15f, -0.5f);
+    public Vector3 M_leftPos = new Vector3(-0.5f, 1f, -0.25f);
+    public Vector3 M_rightPos = new Vector3(0.5f, 1f, -0.25f);
+
+    public float M_transitionTime = 0.5f;
+
+    private enum CameraPosition
+    {
+        left,center,right
+    }
+
+    private CameraPosition m_currentCameraPosition;
+    private bool m_isTransitionning;
+    private float m_transitionValue;
+    private Vector3 m_viewRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_currentCameraPosition = CameraPosition.center;
+        this.transform.localPosition = M_centerPos;
+
+        m_isTransitionning = false;
+        m_transitionValue = 0f;
+        m_viewRotation = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Controller_2_4th_Axis");
+        float vertical = Input.GetAxis("Controller_2_5th_Axis");
+
+        m_viewRotation.y = Mathf.Clamp(m_viewRotation.y + horizontal, -M_maxCameraAngle, M_maxCameraAngle);
+        m_viewRotation.x = Mathf.Clamp(m_viewRotation.x + vertical, -M_maxCameraAngle, M_maxCameraAngle);
+
+        this.transform.localRotation = Quaternion.Euler(m_viewRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float M_MovementSpeed = 1f;
+    public float M_RotationSpeed = 1f;
+
+    private CharacterController m_charController;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_charController = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        float horizontal = Input.GetAxis("Controller_1_X_Axis");
+        float vertical = Input.GetAxis("Controller_1_Y_Axis");
+
+        Vector3 movement = new Vector3(horizontal, 0f, -vertical);
+        movement = this.transform.rotation * movement;
+        movement.Normalize();
+        movement *= Time.deltaTime * M_MovementSpeed;
+        m_charController.Move(movement);
+
+        //Rotation
+        float rotation =  Input.GetAxis("Controller_1_4th_Axis");
+        
+        if(rotation < 0f)
+        {
+            this.transform.Rotate(this.transform.up, -1 * M_RotationSpeed * Time.deltaTime);
+        }
+        else if(rotation > 0f)
+        {
+            this.transform.Rotate(this.transform.up, M_RotationSpeed * Time.deltaTime);
+        }
+    }
+}
