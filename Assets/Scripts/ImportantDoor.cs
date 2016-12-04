b573c30b1bd693bb1ea99dda4d71d124bde295e3
Unknown <patrickmonville@hotmail.com>
2016-12-04 19:46:01 +0100

    implemented victory and defeat (not tested)

---
+++ Assets/Scripts/ImportantDoor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImportantDoor : DoorScript
+{
+	public enum OpenDoorEvent
+	{
+		Victory,
+		Defeat
+	}
+
+	public OpenDoorEvent M_onOpenDoor;
+
+	private GameManager m_gameManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+	}
+
+	public override void ChangeDoorState ()
+	{
+		bool wasDoorOpen = base.isOpen;
+
+		base.ChangeDoorState ();
+
+		if (!wasDoorOpen && base.isOpen) {
+			switch (M_onOpenDoor) {
+			case OpenDoorEvent.Defeat:
+				m_gameManager.OnDoorEvent (M_onOpenDoor);
+				break;
+			case OpenDoorEvent.Victory:
+				m_gameManager.OnDoorEvent (M_onOpenDoor);
+				break;
+			}
+		}
+	}
+}
