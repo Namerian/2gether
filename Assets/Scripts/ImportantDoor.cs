578b137ef9c43d225c503592e9a9c21311e3a688
Nicolas <nicolas-donald.laurent@laposte.net>
2016-12-06 15:41:09 +0100

    Integration WIP

--- Assets/Scripts/ImportantDoor.cs
+++ Assets/Scripts/ImportantDoor.cs
@@ -12,11 +12,15 @@ public class ImportantDoor : DoorScript
 	public OpenDoorEvent M_onOpenDoor;
 
 	private GameManager m_gameManager;
+    private AudioSource m_audio = null;
 
 	// Use this for initialization
 	void Start ()
 	{
 		m_gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+        var audios =  GetComponents<AudioSource>();
+        if (audios.Length > 2)
+            m_audio = audios[2];
 	}
 
 	public override void ChangeDoorState ()
@@ -26,7 +30,8 @@ public class ImportantDoor : DoorScript
 		bool wasDoorOpen = base.isOpen;
 
 		base.ChangeDoorState ();
-
+        if(m_audio != null)
+            m_audio.Play();
 		if (!wasDoorOpen && base.isOpen) {
 			switch (M_onOpenDoor) {
 			case OpenDoorEvent.Defeat:
