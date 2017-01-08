d44984c34992ddf4fb7da983da7ca578321fe4af
Yann <nayplatypusmith@live.fr>
2017-01-08 14:43:34 +0100

    PianoTest Prefab & Scripts

---
+++ Assets/Scripts/NoteScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteScript : InteractableScript {
+	public int noteId;
+	AudioSource note;
+
+	void Start()
+    {
+        note = GetComponent<AudioSource>();
+    }
+
+	public override void Interact()
+    {
+        note.Play();
+		if (this.transform.parent == null)
+            return;
+        GameObject o = this.transform.parent.gameObject;
+
+        var script = o.GetComponent<PianoScript>();
+        if (script != null)
+        {
+            script.addNote(noteId);
+        }
+    }
+
+}
