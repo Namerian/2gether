c2403697976ef0ff4789bf03d5a65b06af296a74
Yann <nayplatypusmith@live.fr>
2017-01-09 20:49:43 +0100

    RecordPlayer Finished

---
+++ Assets/Scripts/RecordPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecordPlayerScript : MonoBehaviour {
+	AudioSource melody;
+	public float timeBetweenSounds = 5.0f;
+
+	void Start () {
+        melody = GetComponent<AudioSource>();
+        StartCoroutine(SoundLoop());
+
+	}
+	
+     IEnumerator SoundLoop(){
+         while(true) {
+             yield return new WaitForSeconds(melody.clip.length + timeBetweenSounds);
+             melody.Play();
+         }
+     }
+}
